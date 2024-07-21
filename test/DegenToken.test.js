5215d898997d556c4b9d3bf2d14e512a83f2de3b
Kycehhh <110338778+Kycehhh@users.noreply.github.com>
2024-07-21 23:43:29 +0800

    Update DegenToken.test.js

--- test/DegenToken.test.js
+++ test/DegenToken.test.js
@@ -1,53 +1,53 @@
 const { expect } = require("chai");
 
-describe("DegenToken", function () {
-  let DegenToken, degenToken, owner, addr1, addr2;
+describe("ShoeToken", function () {
+  let ShoeToken, shoeToken, owner, addr1, addr2;
 
   beforeEach(async function () {
     // Deploy a new contract instance before each test
     [owner, addr1, addr2] = await ethers.getSigners();
-    DegenToken = await ethers.getContractFactory("DegenToken");
-    degenToken = await DegenToken.deploy();
-    await degenToken.deployed();
+    ShoeToken = await ethers.getContractFactory("ShoeToken");
+    shoeToken = await ShoeToken.deploy();
+    await shoeToken.deployed();
   });
 
   it("Should deploy the contract and check initial values", async function () {
-    expect(await degenToken.name()).to.equal("Degen");
-    expect(await degenToken.symbol()).to.equal("DGN");
-    expect(await degenToken.decimals()).to.equal(0);
+    expect(await shoeToken.name()).to.equal("ShoeToken");
+    expect(await shoeToken.symbol()).to.equal("SHOE");
+    expect(await shoeToken.decimals()).to.equal(18);
   });
 
   it("Should allow the owner to mint tokens", async function () {
-    await degenToken.mint(addr1.address, 1000);
-    expect(await degenToken.balanceOf(addr1.address)).to.equal(1000);
+    await shoeToken.mint(addr1.address, ethers.utils.parseUnits("1000", 18));
+    expect(await shoeToken.balanceOf(addr1.address)).to.equal(ethers.utils.parseUnits("1000", 18));
   });
 
-  it("Should allow users to buy clothing items", async function () {
-    await degenToken.mint(addr1.address, 1000);
-    await degenToken.connect(addr1).buyClothingItem(0, 2); // Buying 2 of item ID 0
-    expect(await degenToken.balanceOf(addr1.address)).to.equal(800); // After purchase
-    expect(await degenToken.getUserClothingBalance(addr1.address, 0)).to.equal(2);
+  it("Should allow users to purchase shoe items", async function () {
+    await shoeToken.mint(addr1.address, ethers.utils.parseUnits("1000", 18));
+    await shoeToken.connect(addr1).purchaseShoeItem(0, 2); // Buying 2 of item ID 0
+    expect(await shoeToken.balanceOf(addr1.address)).to.equal(ethers.utils.parseUnits("800", 18)); // After purchase
+    expect(await shoeToken.userShoeBalances(addr1.address, 0)).to.equal(2);
   });
 
-  it("Should allow users to redeem clothing items", async function () {
-    await degenToken.mint(addr1.address, 1000);
-    await degenToken.connect(addr1).buyClothingItem(0, 2);
-    await degenToken.connect(addr1).redeemClothingItem(0, 1); // Redeeming 1 of item ID 0
-    expect(await degenToken.balanceOf(addr1.address)).to.equal(900); // After redeeming
-    expect(await degenToken.getUserClothingBalance(addr1.address, 0)).to.equal(1);
+  it("Should allow users to redeem shoe items", async function () {
+    await shoeToken.mint(addr1.address, ethers.utils.parseUnits("1000", 18));
+    await shoeToken.connect(addr1).purchaseShoeItem(0, 2);
+    await shoeToken.connect(addr1).redeemShoeItem(0, 1); // Redeeming 1 of item ID 0
+    expect(await shoeToken.balanceOf(addr1.address)).to.equal(ethers.utils.parseUnits("900", 18)); // After redeeming
+    expect(await shoeToken.userShoeBalances(addr1.address, 0)).to.equal(1);
   });
 
-  it("Should only allow the owner to add clothing items", async function () {
-    await degenToken.addClothingItem("Virtual Jacket", 250);
-    const item = await degenToken.getClothingItem(3); // ID 3 for the new item
-    expect(item[0]).to.equal("Virtual Jacket");
-    expect(item[1]).to.equal(250);
-    expect(item[2]).to.be.true;
+  it("Should only allow the owner to add shoe items", async function () {
+    await shoeToken.addShoeItem("Formal Shoes", ethers.utils.parseUnits("250", 18));
+    const item = await shoeToken.shoeItems(3); // ID 3 for the new item
+    expect(item.name).to.equal("Formal Shoes");
+    expect(item.price).to.equal(ethers.utils.parseUnits("250", 18));
+    expect(item.available).to.be.true;
   });
 
-  it("Should allow the owner to set clothing item availability", async function () {
-    await degenToken.setClothingItemAvailability(0, false);
-    const item = await degenToken.getClothingItem(0);
-    expect(item[2]).to.be.false;
+  it("Should allow the owner to set shoe item availability", async function () {
+    await shoeToken.setShoeItemAvailability(0, false);
+    const item = await shoeToken.shoeItems(0);
+    expect(item.available).to.be.false;
   });
 });
