48ab7271d3963c526711c2e686da8f348d9227d7
Kycehhh <110338778+Kycehhh@users.noreply.github.com>
2024-08-04 11:20:37 +0800

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -4,31 +4,32 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "hardhat/console.sol";
 
-contract ShoeToken is ERC20, Ownable, ERC20Burnable {
-    struct ShoeItem {
+contract DegenToken is ERC20, Ownable, ERC20Burnable {
+    struct CarItem {
         string name;
         uint256 price;
         bool available;
     }
 
-    ShoeItem[] public shoeItems;
+    CarItem[] public carItems;
 
-    mapping(address => mapping(uint256 => uint256)) public userShoeBalances; // User address => Shoe item ID => Quantity
+    mapping(address => mapping(uint256 => uint256)) public userCarBalances; // User address => Car item ID => Quantity
 
-    constructor() ERC20("ShoeToken", "SHOE") {
-        // Initialize with some shoe items
-        shoeItems.push(ShoeItem("Running Shoes", 100 * 10**decimals(), true));
-        shoeItems.push(ShoeItem("Basketball Shoes", 200 * 10**decimals(), true));
-        shoeItems.push(ShoeItem("Casual Shoes", 150 * 10**decimals(), true));
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
+        // Initialize with some car items
+        carItems.push(CarItem("Sedan", 10000 * 10**decimals(), true));
+        carItems.push(CarItem("SUV", 20000 * 10**decimals(), true));
+        carItems.push(CarItem("Truck", 30000 * 10**decimals(), true));
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+   function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
-    }
+    } 
 
     function decimals() public pure override returns (uint8) {
-        return 18;
+        return 0;
     }
 
     function getBalance() external view returns (uint256) {
@@ -36,34 +37,48 @@ contract ShoeToken is ERC20, Ownable, ERC20Burnable {
     }
 
     function transferTokens(address receiver, uint256 value) external {
-        require(balanceOf(msg.sender) >= value, "You do not have enough Shoe Tokens");
+        require(balanceOf(msg.sender) >= value, "You do not have enough Degen Tokens");
         approve(msg.sender, value);
         transferFrom(msg.sender, receiver, value);
     }
 
     function burnTokens(uint256 value) external {
-        require(balanceOf(msg.sender) >= value, "You do not have enough Shoe Tokens");
+        require(balanceOf(msg.sender) >= value, "You do not have enough Degen Tokens");
         burn(value);
     }
 
-    function addShoeItem(string memory name, uint256 price) public onlyOwner {
-        shoeItems.push(ShoeItem(name, price, true));
-    }
+    function buyCarItem(uint256 itemId, uint256 quantity) external {
+        require(carItems[itemId].available, "Car item not available");
+        uint256 totalPrice = carItems[itemId].price * quantity;
+        require(balanceOf(msg.sender) >= totalPrice, "You do not have enough Degen Tokens");
 
-    function purchaseShoeItem(uint256 itemId, uint256 quantity) external {
-        require(shoeItems[itemId].available, "Shoe item not available");
-        uint256 totalPrice = shoeItems[itemId].price * quantity;
-        require(balanceOf(msg.sender) >= totalPrice, "You do not have enough Shoe Tokens");
-        
         transfer(address(this), totalPrice);
-        userShoeBalances[msg.sender][itemId] += quantity;
+        userCarBalances[msg.sender][itemId] += quantity;
     }
 
-    function redeemShoeItem(uint256 itemId, uint256 quantity) external {
-        require(shoeItems[itemId].available, "Shoe item not available");
-        require(userShoeBalances[msg.sender][itemId] >= quantity, "You do not have enough of this item to redeem");
+    function redeemCarItem(uint256 itemId, uint256 quantity) external {
+        require(carItems[itemId].available, "Car item not available");
+        uint256 totalPrice = carItems[itemId].price * quantity;
+        require(userCarBalances[msg.sender][itemId] >= quantity, "You do not have enough of this item to redeem");
         
-        userShoeBalances[msg.sender][itemId] -= quantity;
-        _mint(msg.sender, shoeItems[itemId].price * quantity);  // Mint tokens back to the user
+        userCarBalances[msg.sender][itemId] -= quantity;
+        _mint(msg.sender, totalPrice);  // Mint tokens back to the user
+    }
+
+    function getCarItem(uint256 itemId) external view returns (string memory, uint256, bool) {
+        CarItem memory item = carItems[itemId];
+        return (item.name, item.price, item.available);
+    }
+
+    function getUserCarBalance(address user, uint256 itemId) external view returns (uint256) {
+        return userCarBalances[user][itemId];
+    }
+
+    function addCarItem(string memory name, uint256 price) public onlyOwner {
+        carItems.push(CarItem(name, price, true));
+    }
+
+    function setCarItemAvailability(uint256 itemId, bool available) public onlyOwner {
+        carItems[itemId].available = available;
     }
 }
